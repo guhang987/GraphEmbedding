8630008879847c5b1250c565ffeb974575b858e6
guhang <610203816@qq.com>
2021-03-03 14:37:24 +0800

    Update node2vec_wiki.py

--- examples/node2vec_wiki.py
+++ examples/node2vec_wiki.py
@@ -11,7 +11,7 @@ from sklearn.manifold import TSNE
 
 
 def evaluate_embeddings(embeddings):
-    X, Y = read_node_label('../data/wiki/wiki_labels.txt')
+    X, Y = read_node_label('../data/wiki/60_label.txt')
     tr_frac = 0.8
     print("Training classifier using {:.2f}% nodes...".format(
         tr_frac * 100))
@@ -20,7 +20,7 @@ def evaluate_embeddings(embeddings):
 
 
 def plot_embeddings(embeddings,):
-    X, Y = read_node_label('../data/wiki/wiki_labels.txt')
+    X, Y = read_node_label('../data/wiki/60_label.txt')
 
     emb_list = []
     for k in X:
